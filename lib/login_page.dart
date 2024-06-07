4dcaeb08efb8cfdc3f82b55705e41493241b30ec
quinn <quinn@runsystem.net>
2024-06-07 09:33:56 +0700

    layout + show/hidden password

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -70,7 +68,97 @@ class PhoneSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const double spaceSize = 7;
+    const double errorTextSize = 10;
+    const double errorTextHeigh = 2;
 
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: 330,
+          height: 50,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+              color: const Color(0xffb4b4b4),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 21, right: 21),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset('assets/united-states.png', width: 30, height: 30),
+                const SizedBox(width: spaceSize),
+                Image.asset(
+                  'assets/arrow-down.png',
+                ),
+                const SizedBox(width: spaceSize),
+                const VerticalDivider(
+                  color: Color(0xffb4b4b4),
+                  width: 2,
+                  indent: 12,
+                  endIndent: 12,
+                ),
+                const SizedBox(width: spaceSize),
+                Expanded(
+                  child: TextField(
+                    controller: TextEditingController(),
+                    decoration: const InputDecoration(
+                      hintText: 'Phone number',
+                      border: InputBorder.none,
+                    ),
+                    style: const TextStyle(
+                      fontFamily: 'PlusJakartaSans',
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700, // 700 extra bold
+                      color: Color(0xff13142e),
+                    ),
+                  ),
+                ),
+                const Icon(
+                  Icons.check_circle_rounded,
+                  color: Color(0xff49E46B),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(left: 12),
+          child: SizedBox(
+            // must be wrapped by SizedBox to preserved the height
+            height: errorTextSize * errorTextHeigh,
+            child: Text(
+              'Invalid phone number or password',
+              style: TextStyle(
+                color: Color(0xffFF0000),
+                fontSize: errorTextSize,
+                fontFamily: 'Popins',
+                height: errorTextHeigh,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PasswordSection extends StatefulWidget {
+  const PasswordSection({super.key});
+
+  @override
+  State<PasswordSection> createState() => _PasswordSectionState();
+}
+
+class _PasswordSectionState extends State<PasswordSection> {
+  bool isShowPassword = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final iconData =
+        isShowPassword ? Icons.visibility : Icons.visibility_off_outlined;
     return Container(
       width: 330,
       height: 50,
@@ -83,60 +171,33 @@ class PhoneSection extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.only(left: 21, right: 21),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Image.asset('assets/united-states.png', width: 30, height: 30),
-            const SizedBox(width: spaceSize),
-            Image.asset(
-              'assets/arrow-down.png',
-            ),
-            const SizedBox(width: spaceSize),
-            const VerticalDivider(
-              color: Color(0xffb4b4b4),
-              width: 2,
-              indent: 12,
-              endIndent: 12,
-            ),
-            const SizedBox(width: spaceSize),
             Expanded(
               child: TextField(
-                controller: TextEditingController(),
+                obscureText: isShowPassword,
                 decoration: const InputDecoration(
-                  hintText: 'Phone number',
+                  hintText: 'Password',
                   border: InputBorder.none,
-                ),
-                style: const TextStyle(
-                  fontFamily: 'PlusJakartaSans',
-                  fontSize: 18,
-                  fontWeight: FontWeight.w700, // 700 extra bold
-                  color: Color(0xff13142e),
                 ),
               ),
             ),
-            const Icon(
-              Icons.check_circle_rounded,
-              color: Color(0xff49E46B),
+            const SizedBox(width: 10),
+            InkWell(
+              overlayColor: MaterialStateProperty.all(Colors.transparent),
+              onTap: () {
+                print('Show password');
+                setState(() {
+                  isShowPassword = !isShowPassword;
+                });
+              },
+              child: Icon(
+                iconData,
+                color: const Color(0xff1d1d1d),
+              ),
             ),
           ],
         ),
       ),
-    );
-  }
-}
-
-class PasswordSection extends StatelessWidget {
-  const PasswordSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        DecoratedBox(
-          decoration: BoxDecoration(color: Colors.green.shade600),
-          child: const SizedBox(width: 330, height: 50, child: Text('Image')),
-        ),
-        const Text('Forgot password'),
-      ],
     );
   }
 }
