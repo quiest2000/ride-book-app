7b2f4b5a3cdfdb1114535f0d9f2bfecbf6b032d9
quinn <quinn@runsystem.net>
2024-06-10 11:41:07 +0700

    refactor: extract input_field

---
+++ lib/input_field.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class InputField extends StatelessWidget {
+  const InputField({
+    super.key,
+    required this.iconData,
+    required this.isShowPassword,
+    required this.onIconPressed,
+    required this.labelText,
+  });
+
+  final IconData iconData;
+  final bool isShowPassword;
+  final String labelText;
+
+  final VoidCallback? onIconPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: isShowPassword,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.only(left: 21),
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Color(0xffb4b4b4),
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+          gapPadding: 4,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          gapPadding: 4,
+        ),
+        labelText: labelText,
+        labelStyle: const TextStyle(
+          fontFamily: 'PlusJakartaSans',
+          fontSize: 14,
+          color: Color(0xffb4b4b4),
+        ),
+        suffixIcon: Padding(
+          padding: const EdgeInsets.only(right: 12, top: 5, bottom: 5),
+          child: IconButton(
+            icon: Icon(iconData),
+            onPressed: onIconPressed,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/password_section.dart
+++ lib/password_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ride_book_app/input_field.dart';
 
 class PasswordSection extends StatefulWidget {
   const PasswordSection({super.key});
@@ -14,47 +15,18 @@ class _PasswordSectionState extends State<PasswordSection> {
   Widget build(BuildContext context) {
     final iconData =
         isShowPassword ? Icons.visibility : Icons.visibility_off_outlined;
-
     return SizedBox(
       width: 330,
       height: 50,
-      child: TextField(
-        obscureText: isShowPassword,
-        decoration: InputDecoration(
-          contentPadding: const EdgeInsets.only(left: 21),
-          border: const OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Color(0xffb4b4b4),
-            ),
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-            gapPadding: 4,
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Theme.of(context).colorScheme.onPrimary,
-            ),
-            borderRadius: const BorderRadius.all(Radius.circular(8)),
-            gapPadding: 4,
-          ),
-          labelText: 'Password',
-          labelStyle: const TextStyle(
-            fontFamily: 'Popins',
-            fontSize: 14,
-            color: Color(0xffb4b4b4),
-          ),
-          suffixIcon: Padding(
-            padding: const EdgeInsets.only(right: 12, top: 5, bottom: 5),
-            child: IconButton(
-              icon: Icon(iconData),
-              // padding: const EdgeInsets.only(right: 21),
-              onPressed: () {
-                setState(() {
-                  isShowPassword = !isShowPassword;
-                });
-              },
-            ),
-          ),
-        ),
+      child: InputField(
+        iconData: iconData,
+        isShowPassword: isShowPassword,
+        labelText: 'Password',
+        onIconPressed: () {
+          setState(() {
+            isShowPassword = !isShowPassword;
+          });
+        },
       ),
     );
   }
